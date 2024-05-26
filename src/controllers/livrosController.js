db36927fd069da102ca1721be1a6ba3d961de571
Jhuli Spindola <jhuspindola@gmail.com>
2024-05-26 17:00:17 -0300

    Novas atualizações!

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -12,7 +12,7 @@ class LivroController {
     } catch (erro) {
       res.status(500).json({ message: "Erro interno no servidor" });
     }
-  }
+  };
 
   static listarLivroPorId = async (req, res) => {
     try {
@@ -26,7 +26,7 @@ class LivroController {
     } catch (erro) {
       res.status(400).send({message: `${erro.message} - Id do livro não localizado.`});
     }
-  }
+  };
 
   static cadastrarLivro = async (req, res) => {
     try {
@@ -38,19 +38,19 @@ class LivroController {
     } catch (erro) {
       res.status(500).send({message: `${erro.message} - falha ao cadastrar livro.`});
     }
-  }
+  };
 
   static atualizarLivro = async (req, res) => {
     try {
       const id = req.params.id;
-
+    
       await livros.findByIdAndUpdate(id, {$set: req.body});
-
+    
       res.status(200).send({message: "Livro atualizado com sucesso"});
     } catch (erro) {
       res.status(500).send({message: erro.message});
     }
-  }
+  };
 
   static excluirLivro = async (req, res) => {
     try {
@@ -62,22 +62,19 @@ class LivroController {
     } catch (erro) {
       res.status(500).send({message: erro.message});
     }
-  }
+  };
 
   static listarLivroPorEditora = async (req, res) => {
     try {
       const editora = req.query.editora;
-
+      
       const livrosResultado = await livros.find({"editora": editora});
 
       res.status(200).send(livrosResultado);
     } catch (erro) {
       res.status(500).json({ message: "Erro interno no servidor" });
     }
-  }
-
-
-
+  };
 }
 
-export default LivroController
+export default LivroController;
